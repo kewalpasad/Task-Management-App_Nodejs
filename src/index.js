8e4151449365fe7c31bdcf96931adfb5c57ca8fb
Kewal Pasad <kewal2123@gmail.com>
2019-06-22 15:39:05 -0400

    login and unique field added to authenticate

--- src/index.js
+++ src/index.js
@@ -1,167 +1,30 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task.js');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-//User CRUD
-
-//User Create
-app.post('/users', async (req, res) => {
-	const user = new User(req.body);
-	try {
-		await user.save();
-		res.status(201).send(user);
-	} catch (e) {
-		res.status(400).send(e);
-	}
-});
-
-//User Read all
-app.get('/users', async (req, res) => {
-	try {
-		const users = await User.find({});
-		res.status(201).send(users);
-	} catch (e) {
-		res.status(500).send();
-	}
-});
-
-//User Read one
-app.get('/users/:id', async (req, res) => {
-	const _id = req.params.id;
-	try {
-		const user = await User.findById(_id);
-		if (!user) {
-			return res.status(404).send();
-		}
-		res.status(200).send(user);
-	} catch (err) {
-		res.status(500).send();
-	}
-});
-
-//User Update
-app.patch('/users/:id', async (req, res) => {
-	const updates = Object.keys(req.body);
-	const allowedUpdates = ['name', 'email', 'password', 'age'];
-	const isValidOperation = updates.every(update =>
-		allowedUpdates.includes(update)
-	);
-
-	if (!isValidOperation) {
-		return res.status(400).send({ error: 'Invalid updates!' });
-	}
-	try {
-		const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-			runValidators: true
-		});
-
-		if (!user) {
-			return res.status(404).send();
-		}
-		res.send(user);
-	} catch (error) {
-		res.status(400).send(error);
-	}
-});
-
-//User Delete
-app.delete('/users/:id', async (req, res) => {
-	const user = await User.findByIdAndDelete(req.params.id);
-	try {
-		if (!user) {
-			return res.status(404).send('No such user found');
-		}
-		res.send(user);
-	} catch (error) {
-		res.status(400).send(error);
-	}
-});
-
-// Task CRUD
-
-//Task Create
-app.post('/tasks', async (req, res) => {
-	const task = new Task(req.body);
-	try {
-		await task.save();
-		res.status(201).send(task);
-	} catch (error) {
-		res.status(400).send(err);
-	}
-});
-
-//Task Read all
-app.get('/tasks', async (req, res) => {
-	try {
-		const task = await Task.find({});
-		res.status(200).send(task);
-	} catch (error) {
-		res.status(500).send();
-	}
-});
-
-//Task Read one
-app.get('/tasks/:id', async (req, res) => {
-	const _id = req.params.id;
-	try {
-		const task = await Task.findById(_id);
-		if (!task) {
-			return res.status(404).send();
-		}
-		res.status(200).send(task);
-	} catch (error) {
-		res.status(500).send();
-	}
-});
-
-//Task Update
-app.patch('/tasks/:id', async (req, res) => {
-	const updates = Object.keys(req.body);
-	const allowedUpdates = ['description', 'completed'];
-	const isValidOperation = updates.every(update =>
-		allowedUpdates.includes(update)
-	);
-
-	if (!isValidOperation) {
-		return res.status(404).send({ error: 'invalid update!' });
-	}
-
-	try {
-		const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-			runValidators: true
-		});
-
-		if (!task) {
-			res.status(404).send({ error: 'no such task found' });
-		}
-
-		res.send(task);
-	} catch (error) {
-		res.status(400).send(error);
-	}
-});
-
-//Task Delete
-app.delete('/tasks/:id', async (req, res) => {
-	const task = await Task.findByIdAndDelete(req.params.id);
-	try {
-		if (!task) {
-			return res.status(404).send('No such task found');
-		}
-		res.send(task);
-	} catch (error) {
-		res.status(400).send(error);
-	}
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
-	console.log('server is up on port' + port);
+	console.log('Server is up on port ' + port);
 });
+
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+	const password = 'Red12345!';
+	const hashedPassword = await bcrypt.hash(password, 8);
+
+	console.log(password);
+	console.log(hashedPassword);
+
+	const isMatch = await bcrypt.compare('red12345!', hashedPassword);
+	console.log(isMatch);
+};
+
+myFunction();
--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: true,
@@ -9,6 +10,7 @@ const User = mongoose.model('User', {
 	},
 	email: {
 		type: String,
+		unique: true,
 		required: true,
 		trim: true,
 		lowercase: true,
@@ -18,28 +20,55 @@ const User = mongoose.model('User', {
 			}
 		}
 	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 7,
+		trim: true,
+		validate(value) {
+			if (value.toLowerCase().includes('password')) {
+				throw new Error('Password cannot contain "password"');
+			}
+		}
+	},
 	age: {
 		type: Number,
 		default: 0,
 		validate(value) {
 			if (value < 0) {
-				throw new Error('Age must be a positive');
-			}
-		}
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 7,
-		validate(value) {
-			if (value.toLowerCase().includes('password')) {
-				throw new Error(
-					'Please change the password as it contains password in it'
-				);
+				throw new Error('Age must be a positive number');
 			}
 		}
 	}
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		throw new Error('Unable to login');
+	}
+
+	const isMatch = await bcrypt.compare(password, user.password);
+
+	if (!isMatch) {
+		throw new Error('Unable to login');
+	}
+
+	return user;
+};
+
+// Hash the plain text password before saving
+userSchema.pre('save', async function(next) {
+	const user = this;
+
+	if (user.isModified('password')) {
+		user.password = await bcrypt.hash(user.password, 8);
+	}
+
+	next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
