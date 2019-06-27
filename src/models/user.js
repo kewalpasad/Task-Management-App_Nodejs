b568084cb759b25f7cf87bd99cfa772801063142
Kewal Pasad <kewal2123@gmail.com>
2019-06-27 16:46:19 -0400

    adding and removing user avatar with auth

--- src/models/user.js
+++ src/models/user.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema(
 			default: 0,
 			validate(value) {
 				if (value < 0) {
-					throw new Error('Age must be a positive number');
+					throw new Error('Age must be a postive number');
 				}
 			}
 		},
@@ -50,7 +50,10 @@ const userSchema = new mongoose.Schema(
 					required: true
 				}
 			}
-		]
+		],
+		avatar: {
+			type: Buffer
+		}
 	},
 	{
 		timestamps: true
